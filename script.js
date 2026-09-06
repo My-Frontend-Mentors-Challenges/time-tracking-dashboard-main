97373c7bd6de24472b2fbf45738e784441b394a5
YoussefAW1 <109309620+YoussefAW1@users.noreply.github.com>
2026-09-06 22:57:29 +0300

    js v2 after format document

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const ulDiv= document.querySelector('ul');
-const timeControls= document.querySelector('.time-controls');
-let activeBtn= document.querySelector('.active');
+const ulDiv = document.querySelector("ul");
+const timeControls = document.querySelector(".time-controls");
+let activeBtn = document.querySelector(".active");
 
-const addToDom= (data)=>{
-    const liDiv= document.createElement('li');
-    liDiv.classList.add(data.title.replace(' ','-').toLowerCase());
-    const timeframes= data.timeframes;
-    const activeBtnText=activeBtn.textContent;
-    liDiv.innerHTML=`
+const addToDom = (data) => {
+  const liDiv = document.createElement("li");
+  liDiv.classList.add(data.title.replace(" ", "-").toLowerCase());
+  const timeframes = data.timeframes;
+  const activeBtnText = activeBtn.textContent;
+  liDiv.innerHTML = `
         <h2>${data.title}</h2>
         <button type="button"><img src="./images/icon-ellipsis.svg" alt="icon ellipsis"></button>
         <span>${timeframes[activeBtnText.toLowerCase()].current} hrs</span>
-        <p>Last ${timeframes[activeBtnText].slice(0,-2)} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
+        <p>Last ${timeframes[activeBtnText].slice(0, -2)} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
     `;
-    ulDiv.appendChild(liDiv);
-}
+  ulDiv.appendChild(liDiv);
+};
 
-const fetchData=()=>{
-fetch('./data.json')
-.then((response)=>response.json())
-.then((dataArray)=>{
-    dataArray.forEach(data => {
+const fetchData = () => {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((dataArray) => {
+      dataArray.forEach((data) => {
         addToDom(data);
+      });
     });
-});
-}
+};
 
-
-timeControls.addEventListener('click',(e)=>{
-    e.target.classList.add('active');
-    activeBtn=e.target;
-    [...timeControls.children].filter(button=>button!=e.target).forEach(button=>{
-        button.classList.remove('active');
+timeControls.addEventListener("click", (e) => {
+  e.target.classList.add("active");
+  activeBtn = e.target;
+  [...timeControls.children]
+    .filter((button) => button != e.target)
+    .forEach((button) => {
+      button.classList.remove("active");
     });
-    fetchData();
+  fetchData();
 });
