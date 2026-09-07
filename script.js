8a0e3b5810e0838a1c37c78506c0721d8eca66a2
YoussefAW1 <109309620+YoussefAW1@users.noreply.github.com>
2026-09-07 11:52:01 +0300

    added img and changed clearing time
    return Response.json first then clear

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ let activeBtn = document.querySelector(".active");
 
 const addToDom = (data) => {
     const liDiv = document.createElement("li");
-    liDiv.classList.add(data.title.replace(" ", "-").toLowerCase());
+    const titleLowerCase= data.title.replace(" ", "-").toLowerCase();
+    liDiv.classList.add(titleLowerCase);
     const timeframes = data.timeframes;
     const activeBtnText = activeBtn.textContent.toLowerCase();
+    
     liDiv.innerHTML = `
         <h2>${data.title}</h2>
         <button type="button"><img src="./images/icon-ellipsis.svg" alt="icon ellipsis"></button>
         <span>${timeframes[activeBtnText.toLowerCase()].current} hrs</span>
         <p>Last ${activeBtnText == "weekly" ? "Week" : activeBtnText == "daily" ? "Day" : "Month"} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
+        <img src="./images/icon-${titleLowerCase}.svg" alt="">
     `;
     ulDiv.appendChild(liDiv);
 };
 
 const fetchData = () => {
     fetch("./data.json")
-        .then((response) => {ulDiv.innerHTML = "";return response.json();})
+        .then((response) => response.json())
         .then((dataArray) => {
+            ulDiv.innerHTML = "";
             dataArray.forEach((data) => {
                 addToDom(data);
             });
