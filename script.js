8d3448f41bbd339ee6dd40d6ba05d7f3e0a8bfde
YoussefAW1 <109309620+YoussefAW1@users.noreply.github.com>
2026-09-07 11:30:38 +0300

    fixed the flicking on rendering li after clicking

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ const timeControls = document.querySelector(".time-controls");
 let activeBtn = document.querySelector(".active");
 
 const addToDom = (data) => {
-  const liDiv = document.createElement("li");
-  liDiv.classList.add(data.title.replace(" ", "-").toLowerCase());
-  const timeframes = data.timeframes;
-  const activeBtnText = activeBtn.textContent;
-  liDiv.innerHTML = `
+    const liDiv = document.createElement("li");
+    liDiv.classList.add(data.title.replace(" ", "-").toLowerCase());
+    const timeframes = data.timeframes;
+    const activeBtnText = activeBtn.textContent.toLowerCase();
+    liDiv.innerHTML = `
         <h2>${data.title}</h2>
         <button type="button"><img src="./images/icon-ellipsis.svg" alt="icon ellipsis"></button>
         <span>${timeframes[activeBtnText.toLowerCase()].current} hrs</span>
-        <p>Last ${timeframes[activeBtnText].slice(0, -2)} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
+        <p>Last ${activeBtnText == "weekly" ? "Week" : activeBtnText == "daily" ? "Day" : "Month"} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
     `;
-  ulDiv.appendChild(liDiv);
+    ulDiv.appendChild(liDiv);
 };
 
 const fetchData = () => {
-  fetch("./data.json")
-    .then((response) => response.json())
-    .then((dataArray) => {
-      dataArray.forEach((data) => {
-        addToDom(data);
-      });
-    });
+    fetch("./data.json")
+        .then((response) => {ulDiv.innerHTML = "";return response.json();})
+        .then((dataArray) => {
+            dataArray.forEach((data) => {
+                addToDom(data);
+            });
+        });
 };
+fetchData();
 
 timeControls.addEventListener("click", (e) => {
-  e.target.classList.add("active");
-  activeBtn = e.target;
-  [...timeControls.children]
-    .filter((button) => button != e.target)
-    .forEach((button) => {
-      button.classList.remove("active");
-    });
-  fetchData();
+    e.target.classList.add("active");
+    activeBtn = e.target;
+    [...timeControls.children]
+        .filter((button) => button != e.target)
+        .forEach((button) => {
+            button.classList.remove("active");
+        });
+    
+    fetchData();
 });
