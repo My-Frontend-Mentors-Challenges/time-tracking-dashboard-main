d7f512c15d8584a049c1b4eea61b371d70ba5e59
YoussefAW1 <109309620+YoussefAW1@users.noreply.github.com>
2026-09-09 22:31:22 +0300

    refactored the rendering algorithm
    rendering data based on the data filter not active element(DOM), one state of truth that controls the fetching and rendering using data attribute

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
 const ulDiv = document.querySelector("ul");
 const timeControls = document.querySelector(".time-controls");
-let activeBtn = document.querySelector(".active");
+const weeklyBtn= timeControls.querySelector('[data-filter="weekly"]');
+let activeFilter = "weekly";
+weeklyBtn.classList.add('active');
 
 const addToDom = (data) => {
     const liDiv = document.createElement("li");
-    const titleLowerCase= data.title.replace(" ", "-").toLowerCase();
+    const titleLowerCase = data.title.replace(" ", "-").toLowerCase();
     liDiv.classList.add(titleLowerCase);
     const timeframes = data.timeframes;
-    const activeBtnText = activeBtn.textContent.toLowerCase();
-    
+
     liDiv.innerHTML = `
         <h2>${data.title}</h2>
         <button type="button"><img src="./images/icon-ellipsis.svg" alt="icon ellipsis"></button>
-        <span>${timeframes[activeBtnText.toLowerCase()].current} hrs</span>
-        <p>Last ${activeBtnText === "weekly" ? "Week" : activeBtnText === "daily" ? "Day" : "Month"} - ${timeframes[activeBtnText.toLowerCase()].previous}hrs</p>
+        <span>${timeframes[activeFilter].current} hrs</span>
+        <p>${activeFilter === "weekly" ? "Last Week" : activeFilter === "daily" ? "Yesterday" : "Last Month"} - ${timeframes[activeFilter].previous}hrs</p>
         <img src="./images/icon-${titleLowerCase}.svg" alt="">
     `;
     ulDiv.appendChild(liDiv);
 };
 
-const fetchData = () => {
-    fetch("./data.json")
-        .then((response) => response.json())
-        .then((dataArray) => {
-            ulDiv.innerHTML = "";
-            dataArray.forEach((data) => {
-                addToDom(data);
-            });
+const fetchData = fetch("./data.json").then((response) => response.json());
+
+const render = () => {
+    fetchData.then((dataArray) => {
+        ulDiv.innerHTML = "";
+        dataArray.forEach((data) => {
+            addToDom(data);
         });
+    });
 };
-fetchData();
+render();
 
 timeControls.addEventListener("click", (e) => {
-    e.target.classList.add("active");
-    activeBtn = e.target;
-    [...timeControls.children]
-        .filter((button) => button != e.target)
-        .forEach((button) => {
-            button.classList.remove("active");
-        });
-    
-    fetchData();
+    const button = e.target.closest("button"); // it might contain another child elements so we choose the closest parent button
+    if (!button) return;
+
+    activeFilter = button.dataset.filter;
+    console.log(activeFilter);
+    [...timeControls.children].forEach((button) => {
+        button.classList.toggle("active", button.dataset.filter === activeFilter);
+    });
+
+    render();
 });
